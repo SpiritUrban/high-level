7f8d501c946d3edc752e9b4105842afb169e7257
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-05-10 01:43:44 +0300

    power log ...in progress

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const l = console.log;
+
+class Log {
+    data = 'xxx';
+    constructor(...args) {
+        this.data = args
+        l('\n');
+        l(...this.data);
+    }
+    err() {
+        l('!!! The Error !!!');
+        return this
+    }
+    con(name) {
+        l('The Controller: ', name);
+        return this
+    }
+    place() {
+        l('Line: 7; File: C/sdfsdf/sdfsdf/sdfsdf/sdfgsdg/sdfgsdg.js');
+        return this
+    }
+}
+
 const self = {
-    log: console.log,
+    log: (...data) => new Log(...data),
 };
 
 module.exports = self;
